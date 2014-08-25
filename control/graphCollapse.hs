f5bcf40fe19d417bc66f466e5a6233a8789e27ed
Martin Drautzburg <Martin.Drautzburg@web.de>
2014-08-25 22:53:45 +0200

    simplifications

--- control/graphCollapse.hs
+++ control/graphCollapse.hs
@@ -36,7 +36,7 @@ type CGraph a = P.Gr (CNode a) CEdge
 n0 :: (Show pl) => Node -> pl -> LNode (CNode pl)
 n0 i pl = (i, N0 lbl pl)
              where
-                 lbl = show pl ++ show i
+                 lbl = show i ++ show pl 
 
 nnEmpty :: Node -> Label -> LNode (CNode pl)
 nnEmpty i lbl = (i, NN lbl [])
@@ -71,6 +71,11 @@ createNode payload = do
 
 createNodes :: (Show pl) => [pl] -> State Int [LNode (CNode pl)]
 createNodes = mapM createNode
+
+------------------------------------------------------------
+-- Connecting Edges
+------------------------------------------------------------
+
 
 connect :: [(LNode (CNode pl),LNode (CNode pl))] -> [LEdge CEdge]
 connect = map newEdge 
@@ -116,7 +121,7 @@ splitIn n xs
 
 
 instance Show ExPayload where
-        show pl = concatMap ( $ pl) [show . pType] 
+        show pl = trc "show" $ " " ++ [pType pl] ++ "\\n" ++ (orig pl) ++ "\\n" ++ (dest pl)
 
 exGraph :: Int -> State Int (CGraph ExPayload)
 exGraph fan = do   
@@ -129,7 +134,7 @@ exGraph fan = do
         walks             = (16*fan) `named` "WK"
         delOffices        = (6*fan)  `named` "DO"
 
-    inUsort  <- createNodes [pl 'T'  ["enters"] ["From"++collOffice] collOffice "CollArrival"
+    coll_usort  <- createNodes [pl 'T'  ["enters"] ["From"++collOffice] collOffice "CollArrival"
                             | collOffice <- collOffices
                             ]
     unpUsort <- createNodes [pl 'U'  ["From"++collOffice] ["From"++collOffice++"Unp"] "CollArrival" "CollArrival"
@@ -138,7 +143,7 @@ exGraph fan = do
     collArr_outw <- createNodes [pl 't'  ["From"++collOffice++"Unp"] ["From"++collOffice++"Unp"] "CollArrival" "OutwArea"
                             | collOffice <- collOffices
                             ]
-    inPsort  <- createNodes [pl 'T'  [center] ["From"++center] center "IctArrival"
+    ctrs_psort  <- createNodes [pl 'T'  [center] ["From"++center] center "IctArrival"
                             | center <- centers
                             ]
     unpPsort <- createNodes [pl 'U'  ["From"++center] myInwGroups "IctArrival" "IctArrival"
@@ -165,27 +170,27 @@ exGraph fan = do
     seq_dept <- createNodes [pl 't'  [walk] [walk] "SeqArea" "DoDeparture"
                             | walk <- walks
                             ]
-    outSeq   <- createNodes [pl 'T'  walk ["For"++delOffice] "DoDeparture" delOffice
+    seq_dos   <- createNodes [pl 'T'  walk ["For"++delOffice] "DoDeparture" delOffice
                             | (delOffice, walk) <- distribute delOffices walks
                             ]
     outw_ict <- createNodes [pl 't'  inwardGroup ["For"++center] "OutwArea" "IctDepart"
                             | (center, inwardGroup) <- distribute centers theirInwGroups
                             ]
-    outPsort <- createNodes [pl 'T'  ["For"++center] ["For"++center] "IctDepart" center
+    psort_ctrs <- createNodes [pl 'T'  ["For"++center] ["For"++center] "IctDepart" center
                             | (center, inwardGroup) <- distribute centers theirInwGroups
                             ]
 
-    let nodes = inUsort
+    let nodes = coll_usort
                 ++ unpUsort
                 ++ collArr_outw
-                ++ inPsort
+                ++ ctrs_psort
                 ++ unpPsort
                 ++ srtOutw
                 ++ outw_ict
-                ++ outPsort
+                ++ psort_ctrs
                 ++ srtInw
                 ++ srtSeq
-                ++ outSeq
+                ++ seq_dos
                 ++ outw_inw
                 ++ inw_seq
                 ++ seq_dept
@@ -200,11 +205,12 @@ exGraph fan = do
           where
               named :: Int -> String -> [String]
               named n s = [s ++ show x | x <- [1..n]]
+
               -- associate groups of ys with xs
               distribute xs ys = zip xs (splitIn (length xs) ys)
 
               intersects s1 s2 = intersect s1 s2 /= []
-              origDest x y     = dest (getNodePayload x) == orig (getNodePayload y)
+              origDest x y     = dest     (getNodePayload x)     ==        orig    (getNodePayload y)
               prodAcc x y      = produces (getNodePayload x) `intersects`  accepts (getNodePayload y)
 
 ------------------------------------------------------------
@@ -259,12 +265,15 @@ byFromToTyped (n, N0 lbl pl) = (n, N0 lbl' pl')
 
 samePayload (_, N0 _ pl1) (_, N0 _ pl2) = pl1 == pl2
 
+-- xxx ugly
 aggregate :: (Eq pl, Ord pl) => CGraph pl -> Aggregator pl -> [(Label, [Node])]
-aggregate gr aggf = map lblNodes $ M.toList $ M.fromListWith (++) triplets
+aggregate gr aggf = map rmPayload $ combine triplets
   where
     triplets = [((lbl, pl),[n]) | (n, N0 lbl pl) <- map aggf $ labNodes gr]
-    lblNodes :: ((Label, pl),[Node]) -> (Label, [Node])
-    lblNodes ((lbl,pl),ns) = (lbl, ns)
+    rmPayload :: ((Label, pl),[Node]) -> (Label, [Node])
+    rmPayload ((lbl,pl),ns) = (lbl, ns)
+    combine :: Ord pl => [((Label,pl),[Node])] -> [((Label,pl),[Node])]
+    combine = M.toList . (M.fromListWith (++))
 
 
 ------------------------------------------------------------
@@ -277,6 +286,9 @@ aggregate gr aggf = map lblNodes $ M.toList $ M.fromListWith (++) triplets
 uniq = nub . sort
 a `without` b = O.minus (sort a)  (sort b)
 
+setOrig, setDest :: Node -> Edge -> Edge
+setOrig n (i,j) = (n,j)
+setDest n (i,j) = (i,n)
 
 type GraphTransform pl = CGraph pl -> CGraph pl
 
@@ -286,49 +298,37 @@ groupNodes graph label ids  =  do
     gr    <- graph
     newId <- nextval 
     let newNode     = aggNodes (newId,label) ids
-        addNewNode  = insNode newNode  
+        addNewNode  = insNode newNode
         delOldNodes = delNodes ids     :: GraphTransform pl
+
         -- Now the edges:
-        oldEdgesTo = do
-            old     <- ids
-            toOld   <- pre gr old
-            return (toOld, old)
-        oldEdgesFrom = do
-            old     <- ids
-            fromOld   <- suc gr old
-            return $ (old, fromOld)
-        oldEdgesWithin = [(i,j) | (i,j,lbl) <- labEdges gr, i `elem` ids, j `elem` ids]
-        newEdges    = uniq $ reconnect 2 newId  (oldEdgesTo   `without`  oldEdgesWithin) ++ 
-                             reconnect 1 newId  (oldEdgesFrom  `without` oldEdgesWithin)
-        delOldEdges = delEdges $ oldEdgesTo ++ oldEdgesFrom  :: GraphTransform pl
+        oldEdgesTo     = [(toOld, old)   | old <- ids, toOld <- pre gr old]
+        oldEdgesFrom   = [(old, fromOld) | old <- ids, fromOld <- suc gr old] 
+        oldEdges       = oldEdgesTo ++ oldEdgesFrom
+        oldEdgesWithin = [ (i,j) | (i,j) <- oldEdges, i `elem` ids, j `elem` ids]
+
+
+        newEdges       = uniq $ map (setDest newId)  (oldEdgesTo   \\ oldEdgesWithin) ++ 
+                                map (setOrig newId)  (oldEdgesFrom \\ oldEdgesWithin)
+        delOldEdges    = delEdges oldEdges  :: GraphTransform pl
         -- beware: inserting edge between nodes wich do not exists causes "Irrefutable pattern failed"
-        addNewEdges = insEdges $ map (uncurry e0) newEdges                :: GraphTransform pl
+        addNewEdges    = insEdges $ map (uncurry e0) newEdges                :: GraphTransform pl
         
     return $ (addNewEdges . delOldEdges . addNewNode . delOldNodes) gr
-            where
-                to (toNode,n,l,f) = toNode
-                from (a,b,c,d) = d
-                reconnect :: Int -> Int -> [Edge] -> [Edge] 
-                reconnect pos id edges  = case pos of
-                  1 -> map (\(i, j) -> (id, j)) edges
-                  2 -> map (\(i, j) -> (i, id)) edges
+
 
 
 exGraph2 :: State Int (CGraph ExPayload)
 exGraph2 = groupNodes (exGraph 1) "foo"  [0,1,2,3,4,12,13,14,15] 
 
 
--- xxx error: trying to insert edge between nodes which do not exist
-exGraph3 :: State Int (CGraph ExPayload)
-exGraph3 = groupNodes' theGraph nodeGroups
-  where
-    -- xxx there must be a way to avoid this explicit recursion
-    theGraph          = (exGraph 4)
-    groupNodes' gr [] = gr
-    groupNodes' gr ((lbl, nodes):groups) = groupNodes' (groupNodes gr lbl nodes) groups
-    nodeGroups        = aggregate g1 byFromToTyped
-    g1                = evalState0 theGraph
-
+exGraph3 :: Int -> State Int (CGraph ExPayload)
+exGraph3 fan = foldr groupNodes' gr nodeGroups
+        where
+            gr = exGraph fan
+            nodeGroups :: [(Label,[Node])]
+            nodeGroups = aggregate (evalState0 gr) byFromToTyped
+            groupNodes' (lbl, ns) g= groupNodes g lbl ns
 
 evalState0  gr = evalState gr 0
     
@@ -339,7 +339,10 @@ evalState0  gr = evalState gr 0
 ------------------------------------------------------------
 draw graph = do
     let g = evalState0 graph
-        dot = "D:/Software/Graphviz/bin/dot"
+--        dot = "D:/Software/Graphviz/bin/dot"
+        dot = "dot"
+        format = "eps"
+        options = " -Gnodesep=0.1  -Nstyle=filled,rounded -Nshape=box -Nfontname=Arial -Efontname=Arial -Epenwidth=3 "
     writeFile "xxx.dot" (graphviz' g)
-    createProcess $ shell $ dot ++ " -T svg -Gnodesep=0.1  -Nstyle=filled,rounded -Nshape=box -Nfontname=Arial -Efontname=Arial -Epenwidth=3 xxx.dot > xxx.svg"
+    createProcess $ shell $ dot ++ " -T " ++ format ++  options  ++ "xxx.dot  > xxx." ++ format
 
