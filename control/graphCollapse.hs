5d5b3a3c4700f2473b122191ac1f780326253dec
Martin Drautzburg <Martin.Drautzburg@web.de>
2014-09-15 20:47:33 +0200

    still refactoring

--- control/graphCollapse.hs
+++ control/graphCollapse.hs
@@ -16,20 +16,12 @@ trc s x = trace (s ++ ":" ++show x) x
 
 type Label = String
 
-data Payload a = PL {label :: String, payload::a}
-               deriving (Eq)
 
-instance Show (Payload a)
-        where
-            show = label
-
-
-
-data CNode pl = N0 (Payload pl) | NN (Payload ())   deriving (Eq)
-instance Show (CNode pl)
+data CNode pl = N0 pl | NN Label   deriving (Eq)
+instance (Show pl) => Show (CNode pl)
         where 
             show (N0 pl) = show pl
-            show (NN pl) = show pl -- ++":"++ (show ns) 
+            show (NN lbl) = lbl
 
 
 data CEdge = E0 Label deriving (Eq)
@@ -46,7 +38,7 @@ type CGraph a = P.Gr (CNode a) CEdge
 
 -- | Node is actually just an Id (i.e. an Int)
 --n0 :: (Show pl) => Node -> (Payload a) -> LNode (CNode pl)
-n0 :: Int -> Payload pl -> LNode (CNode pl)
+n0 :: Int -> pl -> LNode (CNode pl)
 n0 i pl = (i, N0 pl)
 
 
@@ -63,7 +55,7 @@ e0 i j = (i,j, E0 lbl)
 
 
 getNodePayload :: LNode (CNode pl) -> pl
-getNodePayload (id, N0 pl) = payload pl
+getNodePayload (id, N0 pl) = pl
 
 
 ------------------------------------------------------------
@@ -71,23 +63,50 @@ getNodePayload (id, N0 pl) = payload pl
 ------------------------------------------------------------
 type NodePool pl = (Node, [LNode (CNode pl)])
 
-createNode :: (Payload pl) -> State (NodePool pl) ()
-createNode payload = do
-    (id, ns) <- get
-    put (id+1, (n0 id payload):ns)
-    return ()
 
-createNodes :: [Payload pl] -> State (NodePool pl) ()
-createNodes = mapM_ createNode 
+createNode :: DynGraph gr => CNode pl -> State (Node, gr (CNode pl) e) Int
+createNode cn = do
+    (id, gr) <- get
+    let id' = id+1
+        newNode = (id', cn)
+    put (id', insNode newNode gr)
+    return id'
+
+createNodes :: DynGraph gr => [CNode pl] -> State (Node, gr (CNode pl) e) ()
+createNodes = mapM_ createNode
+
+dropNode :: DynGraph gr => Node -> State (Node, gr (CNode pl) e) Int
+dropNode n = do
+    (id, gr) <- get
+    put (id, delNode n gr)
+    return id
+
+dropNodes :: DynGraph gr => [Node] -> State (Node, gr (CNode pl) e) ()
+dropNodes = mapM_ dropNode
+
+createEdge :: DynGraph gr => LEdge e -> State (Node, gr (CNode pl) e) Int
+createEdge e = do
+    (id, gr) <- get
+    put (id, insEdge e gr)
+    return id
+
+createEdges ::DynGraph gr => [LEdge e] -> State (Node, gr (CNode pl) e) ()
+createEdges = mapM_ createEdge
+
+dropEdge :: DynGraph gr => Edge -> State (Node, gr (CNode pl) e) Int
+dropEdge e = do
+    (id, gr) <- get
+    put (id, delEdge e gr)
+    return id
+
+dropEdges :: DynGraph gr => [Edge] -> State (Node, gr (CNode pl) e) ()
+dropEdges = mapM_ dropEdge
+
 
 evalState0  gr = evalState gr 0
 ------------------------------------------------------------
 -- Connecting Edges
 ------------------------------------------------------------
-
-
-connect :: [(LNode (CNode pl),LNode (CNode pl))] -> [LEdge CEdge]
-connect = map newEdge 
 
 newEdge ((i,_), (j,_)) = e0 i j
 
@@ -97,8 +116,6 @@ type NodeEq pl = LNode (CNode pl) -> LNode (CNode pl) -> Bool
 -- | Connect all nodes in nodes which satisfy an equality criterion
 connectAll :: [LNode (CNode pl)] -> NodeEq pl-> [LEdge CEdge]
 connectAll nodes eqf = [newEdge (from, to)| from<-nodes, to<-nodes, eqf from to]
-
-type Projection a b = (Payload a) -> (Payload b) -- xxx
 
 
 ------------------------------------------------------------
@@ -117,18 +134,15 @@ instance Show ExPayload where
         show pl = trc "show" $ " " ++ [pType pl] ++ "\\n" ++ (orig pl) ++ "\\n" ++ (dest pl)
 
 
-pl t a p o d = let pl = Expl{
-                            pType    = t,
-                            accepts  = a,
-                            produces = p,
-                            orig     = o,
-                            dest     = d
-                        }
-               in
-                   PL { label   = show pl,
-                        payload = pl
-                      }
+expl  :: Char -> [String] -> [String] -> String -> String -> CNode ExPayload
 
+expl t a p o d = N0 $ Expl{
+                   pType    = t,
+                   accepts  = a,
+                   produces = p,
+                   orig     = o,
+                   dest     = d
+               }
 
 splitIn :: Int -> [a] -> [[a]]
 splitIn n xs 
@@ -138,9 +152,9 @@ splitIn n xs
                  len   = length xs
                  piece =  ceiling $ (fromIntegral len :: Double)/ fromIntegral n
 
-exGraph :: Int -> State (NodePool ExPayload) (CGraph ExPayload)
+--exGraph :: Int -> State (NodePool ExPayload) (CGraph ExPayload)
           
-
+exGraph  :: DynGraph gr => Int -> State (Node, gr (CNode ExPayload) CEdge) Int
 exGraph fan = do   
     let collOffices       = (2*fan)  `named` "CO"
         centers           = (2*fan)  `named` "CE"
@@ -197,12 +211,14 @@ exGraph fan = do
                  | (center, inwardGroup) <- distribute centers theirInwGroups
                 ]
 
-    (i,nodes) <- get
-    let
-        edges = connectAll nodes connectible
+    (i,gr) <- get
+    let edges     = connectAll (labNodes gr) connectible 
+        grEdged   = foldr insEdge gr edges 
+    put (i, grEdged)
 
 
-    return $ mkGraph nodes edges
+    -- return $ mkGraph nodes edges
+    return i
         where
             named :: Int -> String -> [String]
             named n s = [s ++ show x | x <- [1..n]]
@@ -211,12 +227,12 @@ exGraph fan = do
             distribute xs ys = zip xs (splitIn (length xs) ys)
 
             -- some shorthand commands
-            extTransport products orig dest        = pl 'T' products products orig dest
-            intTransport products orig dest        = pl 't' products products orig dest
-            unpack       accepts produces location = pl 'U' accepts produces location location
-            outwSort     accepts produces location = pl 'O' accepts produces location location
-            inwSort      accepts produces location = pl 'I' accepts produces location location
-            seqSort      accepts produces location = pl 'S' accepts produces location location
+            extTransport products orig dest        = expl 'T' products products orig dest
+            intTransport products orig dest        = expl 't' products products orig dest
+            unpack       accepts produces location = expl 'U' accepts produces location location
+            outwSort     accepts produces location = expl 'O' accepts produces location location
+            inwSort      accepts produces location = expl 'I' accepts produces location location
+            seqSort      accepts produces location = expl 'S' accepts produces location location
 
             -- can two nodes be connected
             intersects s1 s2 = intersect s1 s2 /= []
@@ -239,26 +255,26 @@ exGraph fan = do
 type Aggregator pl = LNode (CNode pl) -> Label
 
 byFrom :: Aggregator ExPayload
-byFrom (n, N0 (PL label pl)) = orig pl
+byFrom (n, N0 pl) = orig pl
 
 byFromTo :: Aggregator ExPayload
-byFromTo (n, N0 (PL lbl pl)) = orig pl ++ "->" ++ dest pl
+byFromTo (n, N0 pl) = orig pl ++ "->" ++ dest pl
 
 
 byFromToTyped :: Aggregator ExPayload
-byFromToTyped (n, N0 (PL lbl pl)) = lbl'
+byFromToTyped (n, N0 pl) = lbl'
   where
     lbl' = [pType pl] ++ ":" ++ (if orig pl == dest pl
                                  then orig pl
                                  else orig pl ++ "->" ++ dest pl
                                 )
 
+-- xxx ugly
 aggregate :: (Eq pl, Ord pl) => CGraph pl -> Aggregator pl -> [(Label, [Node])]
 aggregate gr aggf = map relabel $ groups
   where
     groups     = groupWith aggf (labNodes gr)
     relabel ns = (aggf (head ns), map fst ns)
-
 
 ------------------------------------------------------------
 -- Collapsing and expanding graphs
@@ -275,47 +291,46 @@ setOrig, setDest :: Node -> Edge -> Edge
 setOrig n (i,j) = (n,j)
 setDest n (i,j) = (i,n)
 
-type GraphTransform pl = CGraph pl -> CGraph pl
+--type GraphTransform pl = CGraph pl -> CGraph pl
 
 
-groupNodes :: State Int (CGraph pl) -> (Label,[Node]) ->  State Int (CGraph pl)
-groupNodes graph (lbl,ids)  =  do
-    gr     <- graph
-    (id,_) <- get
-    let addNewNode  = createNode (PL {label=lbl})
-        delOldNodes = delNodes ids     :: GraphTransform pl
 
-        -- Now the edges:
-        oldEdgesTo     = [(toOld, old)   | old <- ids, toOld <- pre gr old]
+groupNodes :: DynGraph gr => (Label,[Node])  -> State (Node, gr (CNode ExPayload) CEdge) Int
+groupNodes (lbl,ids)  =  do
+    aggNode <- createNode (NN lbl)
+    (id',gr) <- get
+    let oldEdgesTo     = [(toOld, old)   | old <- ids, toOld <- pre gr old] 
         oldEdgesFrom   = [(old, fromOld) | old <- ids, fromOld <- suc gr old] 
         oldEdges       = oldEdgesTo ++ oldEdgesFrom
         oldEdgesWithin = [ (i,j) | (i,j) <- oldEdges, i `elem` ids, j `elem` ids]
+        newEdges       = map (uncurry e0) $ uniq $ 
+                                map (setDest id')  (oldEdgesTo   \\ oldEdgesWithin) ++ 
+                                map (setOrig id')  (oldEdgesFrom \\ oldEdgesWithin) 
+    dropNodes ids
+    dropEdges oldEdges
+    createEdges newEdges
+    return id'
 
 
-        newEdges       = uniq $ map (setDest id+1)  (oldEdgesTo   \\ oldEdgesWithin) ++ 
-                                map (setOrig id+1)  (oldEdgesFrom \\ oldEdgesWithin)
-        delOldEdges    = delEdges oldEdges  :: GraphTransform pl
-        -- beware: inserting edge between nodes wich do not exists causes "Irrefutable pattern failed"
-        addNewEdges    = insEdges $ map (uncurry e0) newEdges                :: GraphTransform pl
-        
-    return $ (addNewEdges . delOldEdges . addNewNode . delOldNodes) gr
+exGraph2 :: DynGraph gr => State (Node, gr (CNode ExPayload) CEdge) Int
+exGraph2 = do
+    exGraph 1 
+    groupNodes ("foo",  [0,1,2,3,4,12,13,14,15])
+
 
 
 {-
-exGraph2 :: State Int (CGraph ExPayload)
-exGraph2 = groupNodes (exGraph 1) "foo"  [0,1,2,3,4,12,13,14,15] 
+exGraph3 :: DynGraph gr => Int -> State (Node, gr (CNode ExPayload) CEdge) Int
+exGraph3 fan = do -- foldr groupNodes' gr nodeGroups
+    exGraph fan
+    (i, gr) <- get
+    let x = gr :: P.Gr (CNode ExPayload) CEdge
+--    let nodeGroups = aggregate gr byFromToTyped -- :: [(Label,[Node])]
+--    groupNodes nodeGroups
+    return i
+-}
 
 
-exGraph3 :: Int -> State Int (CGraph ExPayload)
-exGraph3 fan = foldr groupNodes' gr nodeGroups
-        where
-            gr = exGraph fan
-            nodeGroups :: [(Label,[Node])]
-            nodeGroups = aggregate (evalState0 gr) byFromToTyped
-            groupNodes' (lbl, ns) g= groupNodes g lbl ns
-
-
--}    
 
 
 ------------------------------------------------------------
