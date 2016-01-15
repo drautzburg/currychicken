57d4e1b8c6c4e59808627bd8f011d35a7757d338
Martin Drautzburg <Martin.Drautzburg@web.de>
2016-01-15 04:28:45 +0100

    sop operations in xxz.hs

--- opal/xxz.hs
+++ opal/xxz.hs
@@ -1,29 +1,57 @@
 import qualified Data.List as L
+import qualified Data.Set as S
+import qualified Data.Map as M
+import Text.Show.Pretty
+
+
+import Data.Maybe
 import Data.Function
+import Control.Monad
+import Debug.Trace
 
-class Predicate p where
-  sat :: (Eq a, Ord a) => p a -> a -> Bool
+pp x = putStrLn $ show x
 
-class Combinable c where
-  cUnion :: (Eq a, Ord a) => [c a] -> [c a]
+data Lbl a b = Lbl a b 
 
-data Range a b = Range {
-        a :: a,
-        r :: (b,b)
-        }
-               deriving (Eq, Show)
-                        
-instance (Eq a) => Combinable (Range a)
-  where
-    cUnion xs =  map f gs
-      where
-        gs = L.groupBy ((==) `on` a) xs
-        -- [[Range {a = 1, r = (10,20)},Range {a = 1, r = (15,25)}]
-        f ys = foldr1 g ys 
-        g (Range a1 (l1,h1)) (Range a2 (l2,h2)) = Range a1 ((min l1 l2),(max h1 h2))
+type Range a b = M.Map a (S.Set (b,b))
+
+addLbl :: (Ord a, Ord b) => S.Set b -> Lbl a b -> Range a b -> Range a b
+addLbl allBs (Lbl a b) rs = case M.lookup a rs  of
+                                Nothing -> M.insert a (S.singleton (b,b)) rs
+                                Just bs -> case S.lookupLE (b,b) bs of
+                                               Nothing -> undefined
+
+addRange :: (Ord a, Ord b) => S.Set b -> a -> (b,b) -> Range a b -> Range a b
+addRange allBs a (l,h) rs = case M.lookup a rs  of
+                                Nothing -> M.insert a (S.singleton (l,h)) rs
+                                Just bs -> let (++) = S.union
+                                               (below,_)  = S.split (l,l) bs
+                                               (_, above) = S.split (h,h) bs
+                                           in M.insert a (below ++ above ++ S.singleton (l,h)) rs
 
 
-instance Predicate (Range a) where
-  sat (Range a (l,h)) x = x >= l && x <= h
+mergeLower :: S.Set b -> b -> S.Set (b,b) -> S.Set (b,b)
+mergeLower allBs b bs = undefined
 
-xxx = [Range 1 (10,20), Range 1 (15,25), Range 2 (30,40)]
+isGap :: (Ord a, Show a) => S.Set a -> a -> a -> Bool
+isGap xs l h = case S.lookupGT l xs of
+                   Nothing -> True
+                   Just x  -> x >= h
+
+ex_b = S.fromList $ join [[n..n+10] | n <- [0,20..100]]
+ex_r = M.fromList [
+        ("foo",
+              S.fromList[
+                        (1,10),
+                        (20,30)
+                       ]
+        ),
+        ("bar",
+              S.fromList[
+                        (1,10),
+                        (20,30)
+                       ]
+        )
+       ]
+
+xxx = addRange ex_b "foo" (10,12) ex_r
