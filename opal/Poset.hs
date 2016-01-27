982c12209243f8947c06ba0c328b066837e2eece
Martin Drautzburg <Martin.Drautzburg@web.de>
2016-01-27 21:40:31 +0100

    Show for Crust

--- opal/Poset.hs
+++ opal/Poset.hs
@@ -1,6 +1,6 @@
 {-# Language FlexibleInstances #-}
 {-# Language UndecidableInstances #-}
-
+{-# Language FlexibleContexts #-}
 {-
 
 Attempt to see Items not from outside (Containers with their content),
@@ -19,7 +19,9 @@ import qualified Data.List as L
 import qualified Data.Set as S
 import Debug.Trace
 import Test.QuickCheck hiding ((==>))
-
+import GHC.Read
+import Text.ParserCombinators.ReadPrec
+import qualified Text.Read.Lex as L
 ------------------------------------------------------------
 class Poset p where
 ------------------------------------------------------------
@@ -28,19 +30,20 @@ class Poset p where
 -- Instances of Poset
 instance Poset Int where (<:) = (==)
 
+{-
 instance (Eq a, Eq b) => Poset (a,b) 
         where (<:) = (==)
-
+-}
 ------------------------------------------------------------
 -- Alternatives
 ------------------------------------------------------------
 newtype PsList a = PsList [a]
-newtype PsSet  a = PsSet (S.Set a)
+newtype PsSet  a = PsSet (S.Set a) deriving Show
 
 isSubPolist :: (Poset a) => [a] -> [a] ->Bool
-isSubPolist as bs = all included as
+isSubPolist as bs = all inside_some_b as
         where
-            included a = any (a <:) bs
+            inside_some_b a = any (a <:) bs
 
 instance (Eq a, Ord a, Poset a) => Poset (PsList a)
         where
@@ -50,7 +53,7 @@ instance (Eq a, Ord a, Poset a) => Poset (PsSet a)
         where
             (PsSet as) <: (PsSet bs) = isSubPolist (S.toList as) (S.toList bs)
 
-
+{-
 instance (Ord p, Poset p) => Eq (PsList p)
          where as == bs = as <: bs && bs <: as
 
@@ -58,44 +61,78 @@ instance (Ord p, Poset p) => Eq (PsSet p)
          where as == bs = as <: bs && bs <: as
 
 
+instance (Eq a) => Poset a
+        where
+            (<:) = (==)
+-}
+
+{-
 ------------------------------------------------------------
 newtype PsAtom a =  A a
         deriving (Show)
 ------------------------------------------------------------
 -- The only way an atom can <: another atom is to be equal
 instance (Eq a, Ord a) => Poset (PsAtom a) where (<:) (A a) (A b) = a == b
-instance (Eq a, Ord a) => Eq (PsAtom a)    where (==) (A a) (A b) = a == b
-instance (Eq a, Ord a) => Ord (PsAtom a)   where (<=) (A a) (A b) = a <= b
+instance (Eq a, Ord a) => Eq    (PsAtom a) where (==) (A a) (A b) = a == b
+instance (Eq a, Ord a) => Ord   (PsAtom a) where (<=) (A a) (A b) = a <= b
+-}
 
-
+compress :: Ord (PsSet a) => PsSet a -> PsSet (PsSet a)
+compress (PsSet ps) = PsSet $ S.map (PsSet . S.singleton) ps
 ------------------------------------------------------------
 data Crust a = Open [a] | Closed [a]
 ------------------------------------------------------------
-             deriving (Eq, Ord, Show)
+             deriving (Eq, Ord)
+
+
+instance (Show a) => Show (Crust a) where
+        show (Closed []) = "<>"
+        show (Closed (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ ">"
+        show (Open []) = "<..>"
+        show (Open (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ "..>"
+
+
+instance (Read a) => Read (Crust a) where
+        readsPrec _ s = readsCrust s
+
+readsCrust ::  (Read a) => ReadS (Crust a)
+readsCrust ('<':s)       =  do 
+    (x,  t)          <- reads s
+    ((crust, xs), u) <- readsL t
+    return (crust (x:xs),u)
+        where
+            readsL (',':s) = do
+                                (y, t) <- reads s
+                                ((t,ys),u) <- readsL t
+                                return ((t, y:ys), u)
+
+            readsL ('>':s)         = return ((Closed,[]),s) 
+            readsL ('.':'.':'>':s) = return ((Open, []),s)
+
+
+
 
 instance (Eq a, Ord a, Poset a) => Poset (Crust a) 
         where 
             (<:) (Open as) (Closed bs)   = False
             (<:) (Closed as) (Closed bs) = as == bs
 
-            (<:) (Open _) (Open [])         =  True
-            (<:) (Open []) (Open _)         =  False
-            (<:) (Open (x:xs)) (Open (y:ys)) = x <: y && 
-                                               (Open xs) <: (Open ys)
+            (<:) (Open _) (Open [])     =  True
+            (<:) (Open []) (Open _)     =  False
+            (<:) (Open (x:xs)) (Open (y:ys)) 
+                                        = x <: y && 
+                                          (Open xs) <: (Open ys)
 
             (<:) (Closed _) (Open [])   = True
             (<:) (Closed []) (Open _)   = False
-            (<:) (Closed (x:xs)) (Open (y:ys))  = x <: y && 
-                                                  (Closed xs) <: (Open ys)
+            (<:) (Closed (x:xs)) (Open (y:ys))  
+                                        = x <: y && 
+                                          (Closed xs) <: (Open ys)
 
 
 ------------------------------------------------------------
 -- Tests
 ------------------------------------------------------------
-instance Arbitrary (PsAtom Int) where
-        arbitrary = do
-            x <- arbitrary :: (Gen Int)
-            return (A (abs x + 1))
 
 instance (Arbitrary a) => Arbitrary (Crust a)
         where
@@ -106,20 +143,20 @@ instance (Arbitrary a) => Arbitrary (Crust a)
 a ==> b = not a || b 
 infixr 1 ==>
 
-prop_reflexive :: Crust (PsAtom Int) -> Bool
+prop_reflexive :: Crust Int -> Bool
 prop_reflexive as = as <: as
 
-prop_anti :: Crust (PsAtom Int) -> Crust (PsAtom Int) -> Bool
+prop_anti :: Crust Int -> Crust Int -> Bool
 prop_anti as bs = as <: bs && bs <: as ==> as == bs
 
-prop_trans :: Crust (PsAtom Int) -> Crust (PsAtom Int) -> Crust (PsAtom Int) -> Bool
+prop_trans :: Crust Int -> Crust Int -> Crust Int -> Bool
 prop_trans as bs cs = as <: bs && bs <: cs ==> as <: cs
 
 
 onList f (Open as)   = Open   $ f as
 onList f (Closed as) = Closed $ f as
 
-prop_subset :: PsAtom Int -> Crust (PsAtom Int) -> Crust (PsAtom Int) -> Bool
+prop_subset ::  Int -> Crust Int -> Crust Int -> Bool
 prop_subset a as bs 
         | as <: bs = (trace "-- a --") $ as <: (onList (a:) bs)
         | bs <: as = (trace "-- b --") bs <: (onList (a:) as)
@@ -146,3 +183,4 @@ ex_crust2 = [
  Closed [1,2,3],
  Closed [1,2]
  ]
+
--- opal/xxx.hs
+++ opal/xxx.hs
@@ -1,191 +1,57 @@
+{-# Language FlexibleInstances #-}
+{-# Language UndecidableInstances #-}
 
 import qualified Data.List as L
 import qualified Data.Set as S
-import Test.QuickCheck
-import Data.Maybe
-import Text.Show.Pretty
-import Control.Arrow
--- import Data.String.Utils
-pp a = putStrLn $ ppShow a
-lpp a = do
-    putStrLn "\\begin{verbatim}"
-    putStrLn $ ppShow a
-    putStrLn "\\end{verbatim}"
+import Debug.Trace
+import Test.QuickCheck hiding ((==>))
 
+------------------------------------------------------------
+class Poset p where
+------------------------------------------------------------
+        (<:) :: p -> p -> Bool
 
-data Item lty = Inest lty [Item lty] | 
-                Inonempty lty
-              deriving (Eq, Ord, Show)
+-- Instances of Poset
+instance Poset Int where (<:) = (==)
 
+------------------------------------------------------------
+-- Alternatives
+------------------------------------------------------------
+newtype PsList a = PsList [a]
+newtype PsSet  a = PsSet (S.Set a)
 
-class Product prod where
-        accepts :: (Eq lty, Ord lty, Predicate p) => 
-                   prod (p lty) -> Item lty -> Bool
+isSubPolist :: (Poset a) => [a] -> [a] ->Bool
+isSubPolist as bs = all inside_some_b as
+        where
+            inside_some_b a = any (a <:) bs
 
-newtype Foo p lty = Foo {acc :: lty -> Maybe (Foo p lty)}
-xxx :: (Eq lty, Predicate p) => (Foo (p lty)) -> Item lty -> Bool
-xxx (Foo acc) (Inonempty lbl) = case acc lbl of
-                                    Just p -> prAny p
+instance (Eq a, Ord a, Poset a) => Poset (PsList a)
+        where
+            (PsList as) <: (PsList bs) = isSubPolist as bs
 
-
-xxx (Foo acc) (Inest lbl is) = case acc lbl of
-                                   Just p -> all (xxx p) is
+instance (Eq a, Ord a, Poset a) => Poset (PsSet a)
+        where
+            (PsSet as) <: (PsSet bs) = isSubPolist (S.toList as) (S.toList bs)
 
 
 ------------------------------------------------------------
-data Tree a = Tree a [Tree a] | Node a
-               deriving (Eq, Ord, Show)
+data Crust a = Open [a] | Closed [a]
 ------------------------------------------------------------
--- xxx there is no choice, why a class?
--- xxx no need to make Item a newtype
-data ProductRep p lty = ListRep [Tree (p lty)] | 
-                        NestRep (Tree (p lty))
-                                deriving (Show)
+             deriving (Eq, Ord, Show)
 
-class Predicate ls where
-  prSat     :: (Eq lty) => ls lty -> lty -> Bool
-  prAny     :: ls lty -> Bool
-  prShow    :: (Show lty) => ls lty -> String
+instance (Eq a, Ord a, Poset a) => Poset (Crust a) 
+        where 
+            (<:) (Open as) (Closed bs)   = False
+            (<:) (Closed as) (Closed bs) = as == bs
 
--- predicate implemented as a list of labels
-data Lpred lty = Lpred  [lty] | LpAny
+            (<:) (Open _) (Open [])         =  True
+            (<:) (Open []) (Open _)         =  False
+            (<:) (Open (x:xs)) (Open (y:ys)) = x <: y && 
+                                               (Open xs) <: (Open ys)
 
-instance Predicate Lpred where
-  prSat   (Lpred as) a = a `L.elem` as
-  prShow  (Lpred as) = show as
-  prAny LpAny = True
-  prAny _     = False
-
-ex_foo =  Tree "foo" 
-          [
-            Node "foo1",
-            Node "foo2" 
-           ]
+            (<:) (Closed _) (Open [])   = True
+            (<:) (Closed []) (Open _)   = False
+            (<:) (Closed (x:xs)) (Open (y:ys))  = x <: y && 
+                                                  (Closed xs) <: (Open ys)
 
 
-ex_bar =  Tree "bar" []
-
-ex_plist1 = [ex_foo, ex_bar]
-
-
--- The whole Product is a |ListRep|.
-ex_prod1 = ListRep ex_plist1
-
-
-lAccepts :: (Ord lty, Predicate p) => [Tree (p lty)] -> Item lty -> Bool
-lAccepts ps item = any (flip nAccepts $ item) ps
-
-nAccepts :: (Ord lty, Predicate p) => Tree (p lty) -> Item lty -> Bool
-nAccepts (Node p) (Inonempty ilbl)    = prSat p ilbl 
-nAccepts (Tree p ps) (Inest ilbl items) = prSat p ilbl &&
-                                          all (lAccepts ps) items
-nAccepts _ _ = False
-
-
--- instance Product (ProductRep p) where
---        accepts (NestRep p) item = nAccepts p item
---        accepts (ListRep prod) item = lAccepts prod item
-        
-
-{-
-
-lUnion :: Ord lty => [Pitem lty] -> [Pitem lty] -> [Pitem lty]
-lUnion = L.union 
-
-lFilter :: (Ord lty) => Pitem lty -> [Pitem lty] -> [Pitem lty]
-lFilter pn pns = foldr f [] pns
-  where
-    f pn' ys = case nIntersection pn pn' of
-      (Just y') -> y':ys
-      otherwise -> ys
-
-
-lIntersection :: Ord lty => [Pitem lty] -> [Pitem lty] -> [Pitem lty]
-lIntersection pis pls = foldr lUnion [] $ do
-  pck1 <- pis
-  return $ lFilter pck1 pls
-
-
-nIntersection :: (Ord lty) => Pitem lty -> Pitem lty -> Maybe (Pitem lty)
-nIntersection (Pitem a as) (Pitem b bs)
-  | a == b    = Just $ Pitem a (lIntersection as bs) 
-  | otherwise = Nothing
-
--}
-{-
-split :: (Ord a)=> [Plist a] -> Plist a
-split pxs = foldr lUnion (Plist []) pxs
-
-merge :: (Eq a, Ord a) => [Plist a] -> Plist a -> [Plist a]
-merge plss pls = map f plss
-  where
-    f p = lIntersection p pls
-
--- xxx
-mergeAll :: (Eq a, Ord a) => Plist a ->[Pnest a]
-mergeAll (Plist pns) = pns
-
-
-pack :: Pnest lty -> (lty, Plist lty)
-pack (Pnest lbl pls) = (lbl, pls)
-
-unpack :: lty -> Plist lty -> Pnest lty
-unpack lbl pls = Pnest lbl pls
-
-leaf :: lty -> Plist lty
-leaf lbl = Plist[Pnest lbl PlAny]
-
-splitAll :: Ord a => [Pnest a] -> Plist a
-splitAll ns = split $ map (\np -> Plist [np]) ns
-
-type Ex_lbl = (String, String, String)
-ex_truck = 
-        let 
-           --shorthands
-           lbl s x y = s ++ (show x) ++ (show y)
-           clbl s1 s2 n = (s1, s2 ++ (show n), "")
-           range size n = [size*n .. size*(n+1)-1]
-           ------------------------------------------------------------
-           -- Receiver side
-           ------------------------------------------------------------
-           -- The nth route consists of the following addresses
-           -- xxx split ord and prio
-           rRoute route = split [leaf("Letter", lbl "Addr" route i, mclass)
-                                         | i <-[1..10],
-                                               mclass <- ["Ord", "Prio"]
-                               ] :: Plist Ex_lbl
-           -- Each route has its own |Pnest| tray product
-           rTray route   = unpack (clbl "Tray" "Route" route) (rRoute route)      
-                         :: Pnest Ex_lbl
-
-           -- Each delivery office is responsible for 10 routes ...
-           rDof dof    = splitAll [rTray route | route <- range 10 dof] 
-                       :: Plist Ex_lbl
-
-           -- ... and has a dedicated rollcontainer
-           rRc n     = unpack (clbl "RollContainer" "DO" n) (rDof n)  
-                     :: Pnest Ex_lbl
-
-           -- a Region services 5 Delivery Offices ...
-           rRegion reg = splitAll [rRc i | i <- range 5 reg]     
-                       :: Plist Ex_lbl
-
-           -- ... and has a truck bringing the rollcontainers
-           rTruck n  = unpack (clbl "Truck" "Region" n) (rRegion n)   :: Pnest Ex_lbl
-
-           ------------------------------------------------------------
-           -- Now what happens at the departure side of the truck
-           ------------------------------------------------------------
-           -- The truck to region n gets packed
-           sTruck n = pack(rTruck n)               :: (Ex_lbl, Plist Ex_lbl)
-
-           -- Each rollcontainer comes from a different source ...
-           sRcs reg  = mergeAll (snd (sTruck reg))     :: [(Pnest Ex_lbl)]
-
-           -- ... where they get packed
-           sDof reg dof  = pack (sRcs reg !! dof)          :: (Ex_lbl, Plist Ex_lbl)
-
-           -- 
-
-           in (sRcs 0) -- (stray 0) !! 0
--}
