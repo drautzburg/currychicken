3803dc4f6da8539d1e949ee4b81b4d60c791a670
Martin Drautzburg <Martin.Drautzburg@web.de>
2016-01-28 19:17:04 +0100

    Posets

--- opal/Poset.hs
+++ opal/Poset.hs
@@ -17,6 +17,7 @@ the original Crust.
 
 import qualified Data.List as L
 import qualified Data.Set as S
+import Control.Applicative
 import Debug.Trace
 import Test.QuickCheck hiding ((==>))
 import GHC.Read
@@ -26,18 +27,33 @@ import qualified Text.Read.Lex as L
 class Poset p where
 ------------------------------------------------------------
         (<:) :: p -> p -> Bool
+        -- Posets are not Eq, because that would conflict with the
+        -- derived Eq instances
+        equals :: p -> p -> Bool
+        equals a b = a <: b && b <: a
 
--- Instances of Poset
-instance Poset Int where (<:) = (==)
 
-{-
-instance (Eq a, Eq b) => Poset (a,b) 
-        where (<:) = (==)
--}
+
 ------------------------------------------------------------
--- Alternatives
+newtype PsAtom a =  A {val :: a}
+        deriving (Read, Show)
 ------------------------------------------------------------
-newtype PsList a = PsList [a]
+-- PsAtoms are things where <: is the same as ==
+instance (Eq a, Ord a) => Poset  (PsAtom a) where (<:) (A a) (A b) = a == b
+
+instance (Eq a, Ord a) => Eq     (PsAtom a) where 
+        (==) (A a) (A b) = a == b
+instance (Eq a, Ord a) => Ord    (PsAtom a) where (<=) (A a) (A b) = a <= b
+instance                 Functor PsAtom     where fmap f (A a) = A (f a)
+instance Applicative PsAtom where 
+        (A f) <*> (A a) = A (f a)
+        pure f          = A f
+------------------------------------------------------------
+-- Alternatives/Sets
+------------------------------------------------------------
+-- Collections of Posets are Posets themselves
+
+newtype PsList a = PsList [a] deriving (Ord, Eq, Show)
 newtype PsSet  a = PsSet (S.Set a) deriving Show
 
 isSubPolist :: (Poset a) => [a] -> [a] ->Bool
@@ -53,65 +69,16 @@ instance (Eq a, Ord a, Poset a) => Poset (PsSet a)
         where
             (PsSet as) <: (PsSet bs) = isSubPolist (S.toList as) (S.toList bs)
 
-{-
-instance (Ord p, Poset p) => Eq (PsList p)
-         where as == bs = as <: bs && bs <: as
 
-instance (Ord p, Poset p) => Eq (PsSet p)
-         where as == bs = as <: bs && bs <: as
-
-
-instance (Eq a) => Poset a
-        where
-            (<:) = (==)
--}
-
-{-
 ------------------------------------------------------------
-newtype PsAtom a =  A a
-        deriving (Show)
+data Crust a = Open [a] | Closed [a] deriving (Show)
 ------------------------------------------------------------
--- The only way an atom can <: another atom is to be equal
-instance (Eq a, Ord a) => Poset (PsAtom a) where (<:) (A a) (A b) = a == b
-instance (Eq a, Ord a) => Eq    (PsAtom a) where (==) (A a) (A b) = a == b
-instance (Eq a, Ord a) => Ord   (PsAtom a) where (<=) (A a) (A b) = a <= b
--}
-
-compress :: Ord (PsSet a) => PsSet a -> PsSet (PsSet a)
-compress (PsSet ps) = PsSet $ S.map (PsSet . S.singleton) ps
-------------------------------------------------------------
-data Crust a = Open [a] | Closed [a]
-------------------------------------------------------------
-             deriving (Eq, Ord)
+instance Functor Crust where
+        fmap f (Open as)   = Open (fmap f as)
+        fmap f (Closed as) = Closed (fmap f as)
 
 
-instance (Show a) => Show (Crust a) where
-        show (Closed []) = "<>"
-        show (Closed (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ ">"
-        show (Open []) = "<..>"
-        show (Open (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ "..>"
-
-
-instance (Read a) => Read (Crust a) where
-        readsPrec _ s = readsCrust s
-
-readsCrust ::  (Read a) => ReadS (Crust a)
-readsCrust ('<':s)       =  do 
-    (x,  t)          <- reads s
-    ((crust, xs), u) <- readsL t
-    return (crust (x:xs),u)
-        where
-            readsL (',':s) = do
-                                (y, t) <- reads s
-                                ((t,ys),u) <- readsL t
-                                return ((t, y:ys), u)
-
-            readsL ('>':s)         = return ((Closed,[]),s) 
-            readsL ('.':'.':'>':s) = return ((Open, []),s)
-
-
-
-
+-- Crusts of Posets are Posets themselves
 instance (Eq a, Ord a, Poset a) => Poset (Crust a) 
         where 
             (<:) (Open as) (Closed bs)   = False
@@ -129,58 +96,89 @@ instance (Eq a, Ord a, Poset a) => Poset (Crust a)
                                         = x <: y && 
                                           (Closed xs) <: (Open ys)
 
+------------------------------------------------------------
+compress :: Ord (PsSet a) => PsSet a -> PsSet (PsSet a)
+compress (PsSet ps) = PsSet $ S.map (PsSet . S.singleton) ps
+
 
 ------------------------------------------------------------
 -- Tests
 ------------------------------------------------------------
 
-instance (Arbitrary a) => Arbitrary (Crust a)
+instance (Arbitrary a) => Arbitrary (Crust (PsAtom a))
         where
             arbitrary = do
                 xs <- arbitrary 
-                elements [Open xs, Closed xs]
+                elements [
+                         Open $ map A xs, 
+                         Closed $ map A xs
+                        ]
+
 
 a ==> b = not a || b 
 infixr 1 ==>
 
-prop_reflexive :: Crust Int -> Bool
+type IntCrust = Crust (PsAtom Int)
+
+prop_reflexive :: IntCrust -> Bool
 prop_reflexive as = as <: as
 
-prop_anti :: Crust Int -> Crust Int -> Bool
-prop_anti as bs = as <: bs && bs <: as ==> as == bs
 
-prop_trans :: Crust Int -> Crust Int -> Crust Int -> Bool
+prop_anti :: IntCrust -> IntCrust -> Bool
+prop_anti as bs = as <: bs && bs <: as ==> as `equals` bs
+
+prop_trans :: IntCrust -> IntCrust -> IntCrust -> Bool
 prop_trans as bs cs = as <: bs && bs <: cs ==> as <: cs
 
 
 onList f (Open as)   = Open   $ f as
 onList f (Closed as) = Closed $ f as
 
-prop_subset ::  Int -> Crust Int -> Crust Int -> Bool
+
+prop_subset ::  PsAtom Int -> IntCrust -> IntCrust -> Bool
 prop_subset a as bs 
-        | as <: bs = (trace "-- a --") $ as <: (onList (a:) bs)
-        | bs <: as = (trace "-- b --") bs <: (onList (a:) as)
+        | as <: bs = as <: (onList (a:) bs)
+        | bs <: as = bs <: (onList (a:) as)
         | otherwise = True
+
+
+prop_lift :: IntCrust -> IntCrust -> Bool
+prop_lift as bs = (as `equals` bs) ==> (fmap l as) `equals` (fmap l bs)
+        where
+            l x = PsList [x]
 
 
 runTest name test = putStrLn "" >> putStrLn name >> quickCheck test
 
 testAll = do
     runTest "reflexive" prop_reflexive
-    runTest "trans"     prop_trans
-    runTest "anti"      prop_anti
+    runTest "transitive"     prop_trans
+    runTest "antisymmetric"      prop_anti
+    runTest "lift"      prop_lift
+
+
+{-
+instance (Show a) => Show (Crust a) where
+        show (Closed []) = "<>"
+        show (Closed (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ ">"
+        show (Open []) = "<..>"
+        show (Open (x:xs)) = "<" ++ show x ++ foldr ((++) . (","++). show) "" xs ++ "..>"
 
 
 
-
-
-ex_crust1 = [
- Closed [1,2,3],
- Closed [1,2,4]
- ]
-
-ex_crust2 = [
- Closed [1,2,3],
- Closed [1,2]
- ]
-
+instance (Read a) => Read (Crust a) where
+        readsPrec _ s = readsCrust s
+                where
+                    readsCrust ::  (Read a) => ReadS (Crust a)
+                    readsCrust ('<':s)       =  do 
+                                        (x,  t)          <- reads s
+                                        ((crust, xs), u) <- readsL t
+                                        return (crust (x:xs),u)
+                            where
+                                readsL (',':s) = do
+                                                    (y, t) <- reads s
+                                                    ((t,ys),u) <- readsL t
+                                                    return ((t, y:ys), u)
+                                readsL ('>':s)         = return ((Closed,[]),s) 
+                                readsL ('.':'.':'>':s) = return ((Open, []),s)
+-}
